57c42fc2d012b69c28696c3990cd992d9512812a
Rashi-5 <dinekarashmi@gmail.com>
2025-04-12 00:11:10 +0530

    Git reset hard changes

---
+++ SyncSphere/ViewModels/EventManager.swift
@@ -0,0 +1,8 @@
+//
+//  EventManager.swift
+//  SyncSphere
+//
+//  Created by Rashmi Liyanawadu on 2025-04-11.
+//
+
+import Foundation
---
+++ SyncSphere/Views/EventView/EventDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  EventDetailView.swift
+//  SyncSphere
+//
+//  Created by Rashmi Liyanawadu on 2025-04-12.
+//
+
+import SwiftUI
+
+struct EventDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventDetailView()
+}
