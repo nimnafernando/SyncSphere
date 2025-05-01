68687549c1604ab3f2cb0c8f52442dd6fa0d9780
Rashi-5 <dinekarashmi@gmail.com>
2025-05-01 12:58:31 +0530

    Bugfix: Add event with eventUser table function

---
+++ SyncSphere/Components/Common/PopupView.swift
@@ -0,0 +1,18 @@
+//
+//  PopupView.swift
+//  SyncSphere
+//
+//  Created by Rashmi Liyanawadu on 2025-04-30.
+//
+
+import SwiftUI
+
+struct PopupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopupView()
+}
